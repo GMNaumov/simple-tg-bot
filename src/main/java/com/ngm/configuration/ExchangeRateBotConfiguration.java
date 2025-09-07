96fdd007683ecd24b2af67dbf095ee53545dde2b
Georgii Naumov <gmnaumov@yandex.ru>
2025-09-07 21:58:44 +0300

    add service exception class

--- src/main/java/com/ngm/configuration/ExchangeRateBotConfiguration.java
+++ src/main/java/com/ngm/configuration/ExchangeRateBotConfiguration.java
@@ -1,5 +1,6 @@
 package com.ngm.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
@@ -7,12 +8,20 @@ import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 import com.ngm.bot.ExchangeRateBot;
 
+import okhttp3.OkHttpClient;
+
 @Configuration
 public class ExchangeRateBotConfiguration {
     
+    @Bean
     public TelegramBotsApi telegramBotsApi(ExchangeRateBot exchangeRateBot) throws TelegramApiException {
         TelegramBotsApi api = new TelegramBotsApi(DefaultBotSession.class);
         api.registerBot(exchangeRateBot);
         return api;
+    }
+
+    @Bean
+    public OkHttpClient okHttpClient() {
+        return new OkHttpClient();
     }
 }
---
+++ src/main/java/com/ngm/exception/ServiceException.java
@@ -0,0 +1,11 @@
+package com.ngm.exception;
+
+public class ServiceException extends Exception {
+    public ServiceException(String message) {
+        super(message);
+    }
+
+    public ServiceException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
