e809f9a741f4d231c99e63c9c200db5242580551
Georgii Naumov <gmnaumov@yandex.ru>
2025-09-07 21:03:22 +0300

    initial commit

---
+++ src/main/java/com/ngm/simple_tg_bot/SimpleTgBotApplication.java
@@ -0,0 +1,13 @@
+package com.ngm.simple_tg_bot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTgBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleTgBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ngm/simple_tg_bot/SimpleTgBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ngm.simple_tg_bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTgBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
